d596c25d163dfea1deb567a82c834c3389bd00dd
swamysurya <swamysurya1@gmail.com>
2022-09-08 00:12:24 +0530

    second coomit

--- kia.py
+++ kia.py
@@ -1,3 +1,20 @@
-print("hello world")
-print("hello swamy")
-print("hello surya")
+""" accesing vedio footage from mobile camara 
+using an ipweb appliction jthat is downloaded from playstore """
+
+import cv2
+
+cap = cv2.VideoCapture('http://192.168.190.31:8080/video')
+
+while True:
+    ret, frame = cap.read() #read frame/ image one by ine
+    resized = cv2.resize(frame,(600,400))
+    cv2.imshow("frame",resized)
+
+    key = cv2.waitKey(1) # wait till key presses
+    if key == ord("q"): #after pressing the 'q' keyword in key bioard it break the while
+        break
+cap.release() # release vedio capture object
+cv2.destroyAllWindows() # destroy all frame windows
+
+#please do check the lastr two lines of statements outside the loop
+
